5978b7012d688abcb2b99ac45015cf1302e75ffa
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-03-10 15:15:46 +0100

    Added changes on routes api

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{slug}', [ProjectController::class, 'show']);
 
-Route::get('/contacts', [ApiLeadController::class, 'store']);
+Route::post('/contacts', [ApiLeadController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
